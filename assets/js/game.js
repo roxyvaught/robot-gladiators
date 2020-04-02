6a0f909f878c8883cce956866962f0514196f1ae
roxyvaught <roxanna.vaught@gmail.com>
2020-04-02 13:58:15 -0500

    console features

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,7 @@ function fight() {
     window.alert("The fight has begun!");
   }
 
-fight ();
+// fight ();
 
 var playerName = window.prompt("What is your robot's name?");
 
@@ -16,5 +16,3 @@ console.log(10 + 10);
 // what is this?
 console.log("Our robot's name is " + playerName);
 // fight ();
-var name = "your name";
-console.log(name);
